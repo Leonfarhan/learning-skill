9e726e68259e4161e18c926fc14ebaec74db0406
Kevin Adam Prasetya <72896188+kkms51@users.noreply.github.com>
2023-12-08 07:16:52 +0700

    update homesreen

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
